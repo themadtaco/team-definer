474301c62f7dce6db62530a0ea663eb818087bbb
themadtaco <aatrox48@gmail.com>
2022-02-09 21:05:30 -0600

    made employee class and test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,27 @@ test('creates a employee object', () => {
     expect(employee.id).toBe('1');
     expect(employee.email).toBe('Robert@gmail.com');
 });
+
+test('gets employee name', () => {
+    const employee = new Employee('Robert', '1', 'r@gmail.com');
+
+    expect(employee.getName()).toBe('Robert');
+});
+
+test('gets employee id', () => {
+    const employee = new Employee('Robert', '1', 'r@gmail.com');
+
+    expect(employee.getId()).toBe('1');
+});
+
+test('gets employee email', () => {
+    const employee = new Employee('Robert', '1', 'r@gmail.com');
+    
+    expect(employee.getEmail()).toBe('r@gmail.com');
+});
+
+test('gets role', () => {
+    const employee = new Employee('Robert', '1', 'r@gmail.com');
+
+    expect(employee.getRole()).toBe('Employee');
+});
