9f1ac71e3fde47c9bdc4140d8fd6bd266c0d92e5
themadtaco <aatrox48@gmail.com>
2022-02-10 21:29:58 -0600

    added prompts for team members

--- index.js
+++ index.js
@@ -25,22 +25,92 @@ const promptManager = () => {
             name: 'office',
             message: 'What is your office number?'
         }
-    ]);
+    ])
+    .then(data => {
+        const manager = new Manager(data.name, data.employeeId, data.email, data.office);
+        console.log(manager);
+    });
 };
 
-const promptTeam = (teamData) => {
+const promptEngineer = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the engineer's name?"
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the engineer's ID?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the engineer's email?"
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is the engineer's Github"
+            }
+        ])
+        .then(data => {
+            const engineer = new Engineer(data.name, data.id, data.email, data.github);
+            console.log(engineer);
+        });
+};
+
+const promptIntern = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the intern's ID?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the intern's email?"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school did the intern attend?'
+        }
+    ])
+    .then(data => {
+        const intern = new Intern(data.name, data.id, data.email, data.school);
+        console.log(intern);
+    })
+};
+
+const promptTeam = teamData => {
     if (!teamData) {
         teamData = [];
     }
-    return inquirer.prompt([
+    return inquirer.prompt(
         {
             type: 'list',
             name: 'team',
             message: 'What would you like to do?',
             choices: ['Add an Engineer', 'Add an Intern', 'Finish team']
-        }
-    ])
+        })
+        .then(({ team }) => {
+            if(team === 'Add an Engineer') {
+                promptEngineer();
+            } else if(team === 'Add an Intern') {
+                promptIntern();
+            } else{
+
+            }
+        });
 }
 
 promptManager()
-    .then(promptTeam());
+    .then(promptTeam);
